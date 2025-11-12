7dfb72d549f29a16cd90703a43d2aeb114a64848
Don Fernandes <don@cbctech.net>
2025-11-12 13:08:05 -0500

    Update Get-ExtractorSuite.ps1
    need to fix auditing and EOL

--- active_files/Get-ExtractorSuite.ps1
+++ active_files/Get-ExtractorSuite.ps1
@@ -99,6 +99,7 @@ Get-MextModules -ModuleName @('ExchangeOnlineManagement',
 
 
 Connect-AzAccount #-Credential $cred # soon to be deprecated
+# Connect-ExchangeOnline
 Connect-MgGraph -Scopes ("AuditLog.Read.All",
                         "AuditLogsQuery.Read.All",
                         "AuditLogsQuery-Exchange.Read.All",
@@ -125,8 +126,8 @@ Get-MFA -OutputDir $OutputDir
 Get-Users -OutputDir $OutputDir 
 Get-AdminUsers -OutputDir $OutputDir 
 Get-AllRoleActivity -OutputDir $OutputDir 
-Get-GraphEntraSignInLogs -OutputDir $OutputDir -startDate 2025-08-01 #json output of interactive and non-interactive logins
-Get-GraphEntraAuditLogs -OutputDir $OutputDir -startDate 2025-08-01 #json output of Entra audit logs
+Get-GraphEntraSignInLogs -OutputDir $OutputDir -startDate 2025-11-05 #json output of interactive and non-interactive logins
+Get-GraphEntraAuditLogs -OutputDir $OutputDir -startDate 2025-11-05 #json output of Entra audit logs
 Get-ConditionalAccessPolicies -OutputDir $OutputDir
 Get-OAuthPermissionsGraph -OutputDir $OutputDir
 # Get-SecurityAlerts -OutputDir $OutputDir -DaysBack 180 -- not working, think i need the compliance role
@@ -138,8 +139,8 @@ if (-not (Test-Path $OutputDir)) {
 Get-Groups -OutputDir $OutputDir
 Get-DynamicGroups -OutputDir $OutputDir
 Get-GroupMembers -OutputDir $OutputDir
-Get-GroupOwners -OutputDir $OutputDir
-Get-GroupSettings -OutputDir $OutputDir
+#Get-GroupOwners -OutputDir $OutputDir
+#Get-GroupSettings -OutputDir $OutputDir
 
 $OutputDir = 'C:\scripts\Mext\Licenses'
 if (-not (Test-Path $OutputDir)) {
@@ -178,10 +179,10 @@ if (-not (Test-Path $OutputDir)) {
     New-Item -Path $OutputDir -ItemType Directory -Force | Out-Null
 }
 ## AUDIT ##
-Get-DirectoryActivityLogs -OutputDir $OutputDir -StartDate 2025-08-01
-Get-AdminAuditLog -OutputDir $OutputDir -StartDate 2025-08-01
-Get-MailboxAuditLog -OutputDir $OutputDir -StartDate 8/1/2025 -EndDate 8/6/2025
-Get-MessageTraceLog -OutputDir $OutputDir -StartDate 8/1/2025 -EndDate 8/6/2025
+Get-DirectoryActivityLogs -OutputDir $OutputDir -StartDate 2025-11-01
+Get-AdminAuditLog -OutputDir $OutputDir -StartDate 2025-11-01
+Get-MailboxAuditLog -OutputDir $OutputDir -StartDate 11/1/2025 -EndDate 11/12/2025
+Get-MessageTraceLog -OutputDir $OutputDir -StartDate 11/1/2025 -EndDate 11/12/2025
 Get-UALStatistics -OutputDir $OutputDir
 #Get-UALGraph -searchName test3 -startDate "2025-08-01" -endDate "2025-08-02" -OutputDir $OutputDir -Output CSV
 #Get-UALGraph -searchName ualAll -StartDate "2025-08-01" -EndDate "2025-08-02" -OutputDir $OutputDir
