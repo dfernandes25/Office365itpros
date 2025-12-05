7ae1a4033f96c2751536682db07ad96fe353b20a
Don Fernandes <don@cbctech.net>
2025-12-05 14:57:11 -0500

    Add PowerShell script to install Wazuh Agent
    Introduces Install-WazuhAgent.ps1 for automated Wazuh Agent installation on Windows. Supports configuration for both Office Wifi and ZTVPN environments with respective manager IPs.

---
+++ active_files/good-scripts/Install-WazuhAgent.ps1
@@ -0,0 +1,5 @@
+﻿# Office Wifi
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.14.1-1.msi -OutFile $env:tmp\wazuh-agent; msiexec.exe /i $env:tmp\wazuh-agent /q WAZUH_MANAGER='192.168.68.130' 
+
+# ZTVPN
+Invoke-WebRequest -Uri https://packages.wazuh.com/4.x/windows/wazuh-agent-4.14.1-1.msi -OutFile $env:tmp\wazuh-agent; msiexec.exe /i $env:tmp\wazuh-agent /q WAZUH_MANAGER='10.147.17.72' 
